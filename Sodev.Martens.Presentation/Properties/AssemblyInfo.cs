a498edfc204d049e335063e17b87658251f24d14
mrlukz <lukasz.mocarski@gmail.com>
2019-02-15 23:10:37 +0100

    Last changes ;-(

--- Sodev.Martens.Presentation/Properties/AssemblyInfo.cs
+++ Sodev.Martens.Presentation/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sodev.Martens.Presentation")]
+[assembly: AssemblyTitle("RunMarten")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sodev.Martens.Presentation")]
+[assembly: AssemblyProduct("Sodev.Marten.Presentation")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
